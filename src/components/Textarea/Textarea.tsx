8b29c95062992da7bdd37a2fe954213ebe0be808
iozeryakov <94650470+iozeryakov@users.noreply.github.com>
2023-08-04 19:28:57 +0300

    Update Textarea.tsx

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,18 +1,19 @@
 import { FC } from "react";
 import style from "./Textarea.module.css";
 import { ITextarea } from "../../types/components";
+import TextareaAutosize from 'react-textarea-autosize';
 
 export const Textarea: FC<ITextarea> = ({ text, setLastSelect, setValue }: ITextarea) => {
     return (<>
-        <textarea className={style.text} onChange={(e) => {
-            setValue({ ...text, value: e.currentTarget.value })
-            e.currentTarget.style.height = "auto";
-            e.currentTarget.style.height = `${e.target.scrollHeight.toString()}px`;
-            setLastSelect({ id: text.id, index: e.currentTarget.selectionEnd })
-        }}
+        <TextareaAutosize minRows={2} className={style.text}
+            onChange={(e) => {
+                setValue({ ...text, value: e.currentTarget.value })
+                setLastSelect({ id: text.id, index: e.currentTarget.selectionEnd })
+            }}
             onSelect={(e) => setLastSelect({ id: text.id, index: e.currentTarget.selectionEnd })}
             value={text.value}
-        ></textarea >
+        />
+
     </>
     );
 };
